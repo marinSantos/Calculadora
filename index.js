e9b5c6eeb3c6caa20f8c45bd2a11f2a2ce22ffbd
Lucas Marin <96667789+marinSantos@users.noreply.github.com>
2023-02-28 03:07:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
-const main = document.querySelector("main")
-const root = document.querySelector(":root")
-const input = document.getElementById("input")
-const resultInput = document.getElementById("result")
+const input = document.getElementById('input')
+const resultInput = document.getElementById('result')
+const themeSwitcher = document.getElementById('themeSwitcher')
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+const root = document.querySelector(':root')
+const copyToClipboard = document.getElementById('copyToClipboard')
+const main = document.querySelector('main')
 
-document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
-  charKeyBtn.addEventListener("click", function () {
-    const value = charKeyBtn.dataset.value
-    input.value += value
-  })
-})
-
-document.getElementById("clear").addEventListener("click", function () {
-  input.value = ""
-  input.focus()
-})
-
-input.addEventListener("keydown", function (ev) {
-  ev.preventDefault()
-  if (allowedKeys.includes(ev.key)) {
-    input.value += ev.key
-    return
-  }
-  if (ev.key === "Backspace") {
-    input.value = input.value.slice(0, -1)
-  }
-  if (ev.key === "Enter") {
-    calculate()
-  }
-})
-
-document.getElementById("equal").addEventListener("click", calculate)
 
 function calculate() {
   resultInput.value = "ERROR"
@@ -38,32 +13,64 @@ function calculate() {
   const result = eval(input.value)
   resultInput.value = result
   resultInput.classList.remove("error")
+
 }
 
-document.getElementById("copyToClipboard").addEventListener("click", function (ev) {
-  const button = ev.currentTarget
-  if (button.innerText === "Copy") {
-    button.innerText = "Copied!"
-    button.classList.add("success")
-    navigator.clipboard.writeText(resultInput.value)
-  } else {
-    button.innerText = "Copy"
-    button.classList.remove("success")
+input.addEventListener('keydown', function (ev) {
+  ev.preventDefault()
+  copyToClipboard.innerText = 'Copy'
+  copyToClipboard.classList.remove('success')
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key
+    return
   }
+  if (ev.key === 'Backspace') {
+    input.value = input.value.slice(0, -1)
+  }
+  if (ev.key === 'Enter') {
+    calculate()
+
+  }
+
+
 })
 
-document.getElementById("themeSwitcher").addEventListener("click", function () {
-  if (main.dataset.theme === "dark") {
-    root.style.setProperty("--bg-color", "#f1f5f9")
-    root.style.setProperty("--border-color", "#aaa")
-    root.style.setProperty("--font-color", "#212529")
-    root.style.setProperty("--primary-color", "#26834a")
-    main.dataset.theme = "light"
-  } else {
-    root.style.setProperty("--bg-color", "#212529")
-    root.style.setProperty("--border-color", "#666")
-    root.style.setProperty("--font-color", "#f1f5f9")
-    root.style.setProperty("--primary-color", "#4dff91")
-    main.dataset.theme = "dark"
-  }
+document.querySelectorAll('.charKey').forEach(function (item) {
+  item.addEventListener('click', function () {
+    copyToClipboard.innerText = 'Copy'
+    copyToClipboard.classList.remove('success')
+    let value = item.dataset.value
+    input.value += value
+  })
 })
+
+document.getElementById('clear').addEventListener('click', function () {
+  input.value = ''
+  copyToClipboard.innerText = 'Copy'
+  copyToClipboard.classList.remove('success')
+})
+document.getElementById('equal').addEventListener('click', function () {
+  calculate()
+  copyToClipboard.innerText = 'Copy'
+  copyToClipboard.classList.remove('success')
+})
+
+copyToClipboard.addEventListener('click', function () {
+  navigator.clipboard.writeText(resultInput.value)
+
+  copyToClipboard.innerText = 'Sucess!'
+  copyToClipboard.classList.add('success')
+
+  setTimeout(() => {
+    copyToClipboard.innerText = 'Copy'
+    copyToClipboard.classList.remove('success')
+  }, 2000)
+
+
+})
+
+
+
+
+
+
